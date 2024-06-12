10c8f11e8b94ed2cb59339c9a2d6cb464f0f7148
Koen Volkers <k.volkers@wearetriple.com>
2024-06-12 13:07:10 +0200

    Reproduce low latency target issue.

--- BasicPlaybackKotlin/src/main/java/com/bitmovin/player/samples/playback/basic/MainActivity.kt
+++ BasicPlaybackKotlin/src/main/java/com/bitmovin/player/samples/playback/basic/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.bitmovin.player.samples.playback.basic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.bitmovin.analytics.api.AnalyticsConfig
+import androidx.core.view.postDelayed
 import com.bitmovin.player.api.Player
 import com.bitmovin.player.api.analytics.AnalyticsPlayerConfig
+import com.bitmovin.player.api.event.PlayerEvent
+import com.bitmovin.player.api.event.on
+import com.bitmovin.player.api.live.LowLatencyConfig
 import com.bitmovin.player.api.source.SourceConfig
 import com.bitmovin.player.samples.playback.basic.databinding.ActivityMainBinding
 
 private const val Sintel = "https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd"
+
+private const val LL_MANIFEST = "<ll_manifest>"
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,14 +55,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializePlayer() {
-        val analyticsKey = "{ANALYTICS_LICENSE_KEY}"
         player = Player(
             context = this,
-            analyticsConfig = AnalyticsPlayerConfig.Enabled(AnalyticsConfig(analyticsKey)),
-        ).also {
+            analyticsConfig = AnalyticsPlayerConfig.Disabled,
+        ).apply {
+            config.liveConfig.lowLatencyConfig = LowLatencyConfig(
+                targetLatency = 10.0,
+            )
+        }.also {
             binding.playerView.player = it
         }
 
-        player.load(SourceConfig.fromUrl(Sintel))
+        player.on<PlayerEvent.TimeChanged> {
+            Log.d("Low Latency", "latency=${player.lowLatency.latency}s")
+        }
+        player.on<PlayerEvent.StallStarted> {
+            Log.d("Low Latency", "=== stalled ===")
+        }
+
+        player.load(SourceConfig.fromUrl(LL_MANIFEST))
+        player.play()
+
+        binding.playerView.postDelayed(15_000) {
+            Log.d("Low Latency", "=== setting target latency to 10s again ===")
+            player.lowLatency.targetLatency = 10.0
+        }
     }
 }
